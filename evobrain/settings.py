3c9cbbfd64be0ae525b8821ea1c9c80c5ad561fa
Przemyslaw Swierczek <swierq@gmail.com>
2014-01-26 11:06:42 +0100

    first views added

--- evobrain/settings.py
+++ evobrain/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'twitter_bootstrap',
     'south',
+    'evobrain_web', 
 )
 
 MIDDLEWARE_CLASSES = (
--- evobrain/urls.py
+++ evobrain/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'evobrain.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^evobrain/', include('evobrain_web.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
