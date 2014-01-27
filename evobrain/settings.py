7a4e9752aa9ce2ca88f0e81343cdd77c70db14e9
Przemyslaw Swierczek <swierq@gmail.com>
2014-01-27 22:19:29 +0100

    Basic template added, straight from bootstrap page.

--- evobrain/settings.py
+++ evobrain/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jquery',
     'twitter_bootstrap',
     'south',
     'evobrain_web',
--- evobrain_web/views.py
+++ evobrain_web/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Something will appear here soon.")
+    return render(request, 'base.html')
