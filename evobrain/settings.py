fc5bd0580eeccdfc791d0fe2fc966e1664b3ad60
Przemyslaw Swierczek <swierq@gmail.com>
2014-01-27 22:31:47 +0100

    fixing...

--- evobrain/settings.py
+++ evobrain/settings.py
@@ -36,8 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jquery',
-    'twitter_bootstrap',
     'south',
     'evobrain_web',
 )
